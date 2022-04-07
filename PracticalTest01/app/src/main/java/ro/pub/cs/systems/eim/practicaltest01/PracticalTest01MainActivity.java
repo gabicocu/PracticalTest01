09a02e4377faa353daa7300f8c8245033b02beaf
gabicocu <gabrielcoculescu@yahoo.ro>
2022-04-07 12:50:10 +0300

    made listener B

--- PracticalTest01/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01/PracticalTest01MainActivity.java
+++ PracticalTest01/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01/PracticalTest01MainActivity.java
@@ -7,13 +7,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class PracticalTest01MainActivity extends AppCompatActivity {
 
-    private EditText leftEditText;
-    private EditText rightEditText;
-    private Button pressMeButton, pressMeTooButton;
+    private EditText firstEditText;
+    private EditText secondEditText;
+    private TextView operation;
+    private Button addButton, minusButton;
     private Button navigateToSecondaryActivityButton;
 
     @Override
@@ -21,57 +23,60 @@ public class PracticalTest01MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical_test01_main);
 
-//        leftEditText = (EditText) findViewById(R.id.left_edit_text);
-//        rightEditText = (EditText) findViewById(R.id.right_edit_text);
-//
-//        pressMeButton = (Button) findViewById(R.id.press_me_button);
-//        pressMeTooButton = (Button) findViewById(R.id.press_me_too_button);
-//
-//        navigateToSecondaryActivityButton = (Button) findViewById(R.id.navigate_to_secondary_activity_button);
-//
-//        leftEditText.setText(String.valueOf(0));
-//        rightEditText.setText(String.valueOf(0));
-//
-//        pressMeButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                int leftNumberOfClicks = Integer.valueOf(leftEditText.getText().toString());
-//                int rightNumberOfClicks = Integer.valueOf(rightEditText.getText().toString());
-//                leftNumberOfClicks++;
-//                leftEditText.setText(String.valueOf(leftNumberOfClicks));
-//
-//                // service start activation
-////                if (leftNumberOfClicks + rightNumberOfClicks > Constants.NUMBER_OF_CLICKS_THRESHOLD
-////                        && serviceStatus == Constants.SERVICE_STOPPED) {
-////                    Intent intent = new Intent(getApplicationContext(), Test1ModelService.class);
-////                    intent.putExtra(Constants.FIRST_NUMBER, leftNumberOfClicks);
-////                    intent.putExtra(Constants.SECOND_NUMBER, rightNumberOfClicks);
-////                    getApplicationContext().startService(intent);
-////                    serviceStatus = Constants.SERVICE_STARTED;
-////                }
-//
-//            }
-//        });
-//
-//        pressMeTooButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                int rightNumberOfClicks = Integer.valueOf(rightEditText.getText().toString());
-//                int leftNumberOfClicks = Integer.valueOf(leftEditText.getText().toString());
-//                rightNumberOfClicks++;
-//                rightEditText.setText(String.valueOf(rightNumberOfClicks));
-//
-//                // service start activation
-////                if (leftNumberOfClicks + rightNumberOfClicks > Constants.NUMBER_OF_CLICKS_THRESHOLD
-////                        && serviceStatus == Constants.SERVICE_STOPPED) {
-////                    Intent intent = new Intent(getApplicationContext(), Test1ModelService.class);
-////                    intent.putExtra(Constants.FIRST_NUMBER, leftNumberOfClicks);
-////                    intent.putExtra(Constants.SECOND_NUMBER, rightNumberOfClicks);
-////                    getApplicationContext().startService(intent);
-////                    serviceStatus = Constants.SERVICE_STARTED;
-////                }
-//            }
-//        });
+        firstEditText = (EditText) findViewById(R.id.operand_1);
+        secondEditText = (EditText) findViewById(R.id.operand_2);
+
+        addButton = (Button) findViewById(R.id.add_button);
+        minusButton = (Button) findViewById(R.id.minus_button);
+
+        operation = (TextView) findViewById(R.id.operation) ;
+
+        navigateToSecondaryActivityButton = (Button) findViewById(R.id.navigate_to_secondary_activity_button);
+
+        firstEditText.setText(String.valueOf(0));
+       secondEditText.setText(String.valueOf(0));
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int firstOp = Integer.valueOf(firstEditText.getText().toString());
+                int secondOp = Integer.valueOf(secondEditText.getText().toString());
+                int result = firstOp + secondOp;
+                operation.setText(String.valueOf(firstOp) + " + " + String.valueOf(secondOp) + " = " + String.valueOf(result));
+
+                // service start activation
+//                if (leftNumberOfClicks + rightNumberOfClicks > Constants.NUMBER_OF_CLICKS_THRESHOLD
+//                        && serviceStatus == Constants.SERVICE_STOPPED) {
+//                    Intent intent = new Intent(getApplicationContext(), Test1ModelService.class);
+//                    intent.putExtra(Constants.FIRST_NUMBER, leftNumberOfClicks);
+//                    intent.putExtra(Constants.SECOND_NUMBER, rightNumberOfClicks);
+//                    getApplicationContext().startService(intent);
+//                    serviceStatus = Constants.SERVICE_STARTED;
+//                }
+
+            }
+        });
+
+        minusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int firstOp = Integer.valueOf(firstEditText.getText().toString());
+                int secondOp = Integer.valueOf(secondEditText.getText().toString());
+                int result = firstOp - secondOp;
+                operation.setText(String.valueOf(firstOp) + " - " + String.valueOf(secondOp) + " = " + String.valueOf(result));
+
+                // service start activation
+//                if (leftNumberOfClicks + rightNumberOfClicks > Constants.NUMBER_OF_CLICKS_THRESHOLD
+//                        && serviceStatus == Constants.SERVICE_STOPPED) {
+//                    Intent intent = new Intent(getApplicationContext(), Test1ModelService.class);
+//                    intent.putExtra(Constants.FIRST_NUMBER, leftNumberOfClicks);
+//                    intent.putExtra(Constants.SECOND_NUMBER, rightNumberOfClicks);
+//                    getApplicationContext().startService(intent);
+//                    serviceStatus = Constants.SERVICE_STARTED;
+//                }
+
+            }
+        });
 //
 //        navigateToSecondaryActivityButton.setOnClickListener(new View.OnClickListener() {
 //            @Override
